22d9464513ed86f27affabb95ed471522dba7998
Nnamdi Ugwuegbu <nnamdi@biosec.com.ng>
2025-05-14 03:22:48 +0100

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Medical Chatbot",
+    version = "0.0.0",
+    author = "Nnamdi Ugwuegbu",
+    author_email = "nugwuegbu089@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
